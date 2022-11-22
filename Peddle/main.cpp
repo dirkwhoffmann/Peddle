622586dc02fcd72b01b9233e27474fe86daf4d09
Dirk Hoffmann <dirk.hoffmann@me.com>
2022-11-22 18:19:10 +0100

    Initial Commit

---
+++ Peddle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Peddle
+//
+//  Created by Dirk Hoffmann on 22.11.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
